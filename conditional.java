d5e81ed55f376487722ed743cf1364d673cf3fef
Abhishek Shinde <abhishekshinde5805@gmail.com>
2025-08-15 21:12:38 +0530

    Update conditional.java

--- conditional.java
+++ conditional.java
@@ -1,13 +1,31 @@
 public class conditional {
     public static void main(String[]args)
     {
-        int a=1;
-        int b=13;
-        if(a>b)
-            System.out.println("a is largest"+a);
-        else
-            System.out.println("b is largest"+b);
+         // int a=1;
+         // int b=13;
+        // if(a>b)
+        //     System.out.println("a is largest"+a);
+        // else
+        //     System.out.println("b is largest"+b);
 
+        // if else 
+        int marks=55;
+        
+        if(marks>80)
+        {
+            System.out.println("distinct");
+        }
+        else if(marks>70)
+        {
+           System.out.println("first class"); 
+        }
+        else
+        {
+            System.out.println("Pass"); 
+        }
+            
+            
 
     }
 }
+
