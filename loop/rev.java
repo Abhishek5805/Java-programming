4e196297a9c97ffb6943b06a180beba24a410dcb
Abhishek Shinde <abhishekshinde5805@gmail.com>
2025-08-19 20:01:54 +0530

    Add files via upload

---
+++ loop/rev.java
@@ -0,0 +1,13 @@
+public class rev {
+    public static void main(String[]args)
+{
+    int n=12546;
+    while(n>0)
+    {
+        int lastdigit=n%10;
+        System.out.print(lastdigit);
+        n=n/10;
+    }
+    System.out.println();
+}
+}
