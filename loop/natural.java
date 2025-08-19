4429a4ac1348e6ef2454c5ded6978359e69a5ad3
Abhishek Shinde <abhishekshinde5805@gmail.com>
2025-08-19 11:22:55 +0530

    Add files via upload

---
+++ loop/natural.java
@@ -0,0 +1,21 @@
+package loop;
+
+import java.util.Scanner;
+
+public class natural {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        int i=1;
+        while(i<=n)
+        {
+           sum=sum+i;
+           i++;
+            
+        }
+        System.out.println("sum of natural number is"+sum);
+    }
+    
+}
---
+++ loop/printnum.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class printnum {
+    public static void main(String[]args)
+    {
+        int number=1;
+        while(number<=10)
+        {
+            System.out.println("number is"+number);
+            number++;
+        }
+    }
+}
